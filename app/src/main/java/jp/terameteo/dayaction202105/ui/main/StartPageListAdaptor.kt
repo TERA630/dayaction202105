3becb89646d2d267c29757d8ba938d09527ca246
TERA630 <tera.bcool.4475@gmail.com>
2021-06-03 23:27:15 +0900

    second commit

--- app/src/main/java/jp/terameteo/dayaction202105/ui/main/StartPageListAdaptor.kt
+++ app/src/main/java/jp/terameteo/dayaction202105/ui/main/StartPageListAdaptor.kt
@@ -9,26 +9,28 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import jp.terameteo.dayaction202105.ItemEntity
 import jp.terameteo.dayaction202105.R
+import jp.terameteo.dayaction202105.databinding.ItemTestBinding
 
 class StartPageListAdaptor (private val viewLifecycleOwner: LifecycleOwner, private val viewModel: PageViewModel):
-    RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    androidx.recyclerview.widget.ListAdapter<ItemEntity, StartPageListAdaptor.ViewHolderOfCell>(DiffCallback) {
     override fun getItemCount(): Int = viewModel.currentItems. size
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_test,parent,false)
-        return ViewHolderOfCell(itemView)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderOfCell {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return ViewHolderOfCell(ItemTestBinding.inflate(layoutInflater,parent,false))
     }
-    class ViewHolderOfCell(_cellView: View) : RecyclerView.ViewHolder(_cellView){
-        val textView: TextView = _cellView.findViewById(R.id.cellText)
-    }
+    class ViewHolderOfCell(private val binding:ItemTestBinding) : RecyclerView.ViewHolder(binding.root) {
+        val textView: TextView = binding.cellText
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+
+    }
+    override fun onBindViewHolder(holder: ViewHolderOfCell, position: Int) {
         val thisPositionViewHolder = holder as ViewHolderOfCell
         val thisPositionView = thisPositionViewHolder.textView
         thisPositionView.text =  viewModel.currentItems[position].title
     }
 }
-private object DiffCallback : DiffUtil.ItemCallback<ItemEntity>() {
+    private object DiffCallback : DiffUtil.ItemCallback<ItemEntity>() {
     override fun areItemsTheSame(oldItem: ItemEntity, newItem: ItemEntity): Boolean {
         return oldItem.id == newItem.id
     }
