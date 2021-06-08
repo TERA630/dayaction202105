a474f042289aa73cb68cd0d2392d6f74d129722e
Yoshihiko <tera.bcool.4475@gmail.com>
2021-06-08 13:31:55 +0900

    Document change

--- app/src/main/java/jp/terameteo/dayaction202105/ui/main/MainViewModel.kt
+++ app/src/main/java/jp/terameteo/dayaction202105/ui/main/MainViewModel.kt
@@ -12,11 +12,6 @@ class MainViewModel : ViewModel() {
     lateinit var currentReward:MutableLiveData<Int>
 
 
-    fun loadModel(_context: Context){
-
-    }
-
-
     fun convertItemToString(StoredItemEntity: StoredItemEntity): String { // ItemEntity が変われば直す必要あり
         return StoredItemEntity.title + ";" + StoredItemEntity.reward + ";" + StoredItemEntity.category
     }
@@ -47,3 +42,17 @@ class MainViewModel : ViewModel() {
     }
 
 }
+
+// ViewModel 　Viewの状態保持
+//　回転やアプリ切り替えなどでも破棄されない､Activityとは別のLifecycleを持つ｡
+//　LiveDataを保持する｡
+//　Modelのデータから､ViewにUIを描画する(Binding)ための情報を渡す｡
+//　Activity､Fragment､Adapterのいずれにも渡せるはず｡
+//　LiveDataを保持する｡　ActivityやFragmentはObserveして変更があればUI更新
+//　AdapterはViewModelを参照している｡　Observeするならば･･
+//　Viewへの参照を保持するべきでない｡
+//　Context/Activityの参照を保持するべきでない｡
+//　ViewへのActionを送信｡　Commands
+//　アンチパターン　ViewがViewModelのメンバを直接操作
+//　Model-> ViewModel　ModelからUIの描画に必要な情報に変換保持
+//　ViewからのActionをModelに通知｡
